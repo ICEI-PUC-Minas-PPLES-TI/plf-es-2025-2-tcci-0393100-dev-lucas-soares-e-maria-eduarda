99309f94a106c19c012723a3f4418816dca738d8
Lucas Soares <polonio230@hotmail.com>
2026-03-04 17:51:36 -0300

    feat: initial React + Vite setup

---
+++ Codigo/front-end/src/App.tsx
@@ -0,0 +1,15 @@
+import {Card}from "./components/Card.tsx";
+
+function App() {
+
+  return (
+      <>
+          <div>
+              GraphTest
+              <Card />
+          </div>
+      </>
+  )
+}
+
+export default App
